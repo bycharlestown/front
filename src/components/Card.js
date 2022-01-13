c4a786584e8c19e6e95e7ac2822a38e385216949
traffichouse <github@traffichouse.xyz>
2022-01-13 17:06:54 +0300

    fex number render

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 
 const Card = ({ card }) => {
+  console.log(card.price_min);
   return (
     <li className="card">
       <div className="card__image-wrapper">
@@ -8,7 +9,13 @@ const Card = ({ card }) => {
       </div>
       <div className="card__text-container">
         <h2 className="card__title">{card.title}</h2>
-        <p className="card__price">{card.price}</p>
+        <p className="card__price">
+          {Number(card.price_min).toLocaleString()}
+          {card.price_min === card.price_max
+            ? ""
+            : ` — ${Number(card.price_max).toLocaleString()}`}{" "}
+          ₽
+        </p>
         <p className="card__description">{card.description}</p>
       </div>
       <NavLink className="card__link" to={`/cards/${card.id}`}>
