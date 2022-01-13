ee818e5a641ded26b1a05e25c7ef0a5ddb4d83e7
traffichouse <github@traffichouse.xyz>
2022-01-13 16:47:18 +0300

    create category section

--- src/components/SortSearch.js
+++ src/components/SortSearch.js
@@ -1,12 +1,18 @@
 import { NavLink } from "react-router-dom";
 
 const SortSearch = ({ allCategories }) => {
-  let categories = [...new Set(allCategories)];
+  let categories = [];
+
+  for (let card of allCategories) {
+    categories.push(card.category);
+  }
+
+  categories = [...new Set(categories)];
   return (
     <div className="sorting">
-      {categories.map((category, index) => (
-        <NavLink className="franchise__link" key={category} to={`/${category}`}>
-          {category}---{index}
+      {categories.map((category) => (
+        <NavLink className="sorting__link" key={category} to="/:category">
+          {category}
         </NavLink>
       ))}
     </div>
